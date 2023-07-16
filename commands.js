fc93851e098b4c4b8ce69844f483f403626b95c0
aminatol <aminatoluwo@gmail.com>
2023-07-15 22:15:49 -0400

    added additional queries and updates

--- commands.js
+++ commands.js
@@ -25,14 +25,15 @@ db.bounties.find({reward:{$all:[10000]}})
 // 2. Query for all bounties with a reward worth 10000 or more
 
 // 3. Query for all bounties, but exclude the client attribute from being shown
-
+db.bounties.updateMany({client:"Red wolf"},{$set:{client:0}})
 // 4. Query for a Groundhog in the Woodlands
-
+db.bounties.find({species:"Groundhog"})
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
-
+db.bounties.updateOne({name:"Polarwind"},{$set:{reward:10000}})
 // 2. Remove Lokinkajou
-
+db.bounties.deleteOne({name:"Lokinkajou"})
 // 3. Delete all bounties sent by Songbird
-
+db.bounties.deleteMany({client:{$all:["Songbird"]}})
 // 4. Update all captured statuses to true
+db.bounties.updateMany({captured:false},{$set:{captured:true}})
